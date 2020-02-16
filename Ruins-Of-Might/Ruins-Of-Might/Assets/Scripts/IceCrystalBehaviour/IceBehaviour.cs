a62adfe2c03ffec951a265d108e4643929861323
KarlBaecklund <46314156+KarlBaecklund@users.noreply.github.com>
2020-02-16 20:51:44 +0100

    Updated Ice Crystal

--- Ruins-Of-Might/Ruins-Of-Might/Assets/Scripts/IceCrystalBehaviour/IceBehaviour.cs
+++ Ruins-Of-Might/Ruins-Of-Might/Assets/Scripts/IceCrystalBehaviour/IceBehaviour.cs
@@ -4,17 +4,29 @@ using UnityEngine;
 
 public class IceBehaviour : MonoBehaviour
 {
-    [SerializeField] frozenGround m_frozenGround;
 
+    //public LayerMask m_layerMask;
+
+    [SerializeField] public PhysicsMaterial2D m_matFrozen;
+    //[SerializeField] public PhysicsMaterial2D m_matNotFrozen;
     private void OnTriggerEnter2D(Collider2D collider) {
-        //Debug.Log("ENTER");
-        m_frozenGround.Freezing();
+        
+        if (collider.gameObject.layer.Equals(11)) {
+            Freezing(collider);
+            }
+        
      }
-
-    private void OnTriggerExit2D(Collider2D collider) {
-        //Debug.Log("EXIT");
-        m_frozenGround.NotFreezing();
+    //private void OnTriggerExit2D(Collider2D collider) {
+       
+    //    }
+    private void Freezing(Collider2D collider) {
+        collider.gameObject.GetComponent<BoxCollider2D>().sharedMaterial = m_matFrozen;
+        collider.gameObject.GetComponent<SpriteRenderer>().color = new Color(1, 0, 0);
         }
-}
+    //private void NotFreezing(Collider2D collider) {
+    //    collider.gameObject.GetComponent<BoxCollider2D>().sharedMaterial = m_matNotFrozen;
+    //    collider.gameObject.GetComponent<SpriteRenderer>().color = new Color(0, 1, 0);
+    //    }
+    }
 
 
