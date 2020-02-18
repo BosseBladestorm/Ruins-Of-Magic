d7c6e3fad282454178a75df583f2a79203ab6300
KarlBaecklund <46314156+KarlBaecklund@users.noreply.github.com>
2020-02-18 11:46:08 +0100

    Usar friendly

--- Ruins-Of-Might/Ruins-Of-Might/Assets/Scripts/IceCrystalBehaviour/IceBehaviour.cs
+++ Ruins-Of-Might/Ruins-Of-Might/Assets/Scripts/IceCrystalBehaviour/IceBehaviour.cs
@@ -2,32 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IceBehaviour : MonoBehaviour
-{
+public class IceBehaviour : MonoBehaviour {
 
-    public LayerMask m_layerMaskIgnore;
-
-    [SerializeField] public PhysicsMaterial2D m_matFrozen;
-    //[SerializeField] public PhysicsMaterial2D m_matNotFrozen;
+    [SerializeField] LayerMask m_layerMask;
+    [SerializeField] PhysicsMaterial2D m_matFrozen;
     private void OnTriggerEnter2D(Collider2D collider) {
-        Debug.Log(collider.name);
-        RaycastHit2D hit2D = Physics2D.Linecast(transform.position, collider.transform.position, ~m_layerMaskIgnore.value);
-        if (hit2D) {
-                Freezing(collider);
-                }
-            
-     }
-    //private void OnTriggerExit2D(Collider2D collider) {
-       
-    //    }
-    private void Freezing(Collider2D collider) {
-        collider.gameObject.GetComponent<BoxCollider2D>().sharedMaterial = m_matFrozen;
-        collider.gameObject.GetComponent<SpriteRenderer>().color = new Color(1, 0, 0);
+        var hit = Physics2D.Linecast(this.transform.position, collider.transform.position, m_layerMask);
+        if (hit) {
+            Freezing(hit.transform.gameObject);
         }
-    //private void NotFreezing(Collider2D collider) {
-    //    collider.gameObject.GetComponent<BoxCollider2D>().sharedMaterial = m_matNotFrozen;
-    //    collider.gameObject.GetComponent<SpriteRenderer>().color = new Color(0, 1, 0);
-    //    }
     }
+    private void Freezing(GameObject obj) {
+
+        obj.GetComponent<BoxCollider2D>().sharedMaterial = m_matFrozen;
+        obj.GetComponent<SpriteRenderer>().color = new Color(1, 0, 0);
+    }
+
+}
 
 
