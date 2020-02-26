e0ca91b2f491c4fe511d46a5956ba51887874bb9
Elias Herman <elias.herman01@gmail.com>
2020-02-26 12:20:15 +0100

    crystal player fix for Robert

---
+++ Ruins-Of-Might/Ruins-Of-Might/Assets/Scripts/MagicBeamBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicBeamBehaviour : MonoBehaviour {
+
+    [SerializeField] Transform m_pivot;
+    [SerializeField] SpriteRenderer m_spriteRenderer;
+    private Vector3 m_target;
+    private float m_spriteWidth;
+
+    private void Start() {
+        m_spriteWidth = m_spriteRenderer.sprite.rect.width;
+
+    }
+
+    private void Update() {
+        
+
+
+    }
+
+    public void ScaleBeam() {
+        float scaleY = Vector2.Distance(m_target, m_pivot.position);
+        transform.localScale = new Vector3((scaleY / m_beamSpriteSize) / m_beam.transform.localScale.x, 1, 1);
+
+    }
+
+}
