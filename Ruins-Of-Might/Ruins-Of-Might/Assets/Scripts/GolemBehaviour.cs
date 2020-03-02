ea1279ea7ccddbc04bbbf667d776dccad94abab7
KarlBaecklund <46314156+KarlBaecklund@users.noreply.github.com>
2020-03-02 09:11:40 +0100

    Golem rework

--- Ruins-Of-Might/Ruins-Of-Might/Assets/Scripts/GolemBehaviour.cs
+++ Ruins-Of-Might/Ruins-Of-Might/Assets/Scripts/GolemBehaviour.cs
@@ -8,6 +8,7 @@ public class GolemBehaviour : CrystalBase
 
     [SerializeField] Transform[] wanderPoints;
     [SerializeField] Transform currentWanderPoint;
+    [SerializeField] LayerMask pickupable;
     [SerializeField] int wanderPointIndex;
     [SerializeField] bool isActive;
     private int indexDirection;
@@ -16,8 +17,10 @@ public class GolemBehaviour : CrystalBase
     [SerializeField] Rigidbody2D m_rigidbody;
     [SerializeField] Transform m_transform;
     // Start is called before the first frame update
+
     void Start()
     {
+        
         if (m_rigidbody == null) {
             m_rigidbody = GetComponent<Rigidbody2D>();
         }
@@ -31,10 +34,22 @@ public class GolemBehaviour : CrystalBase
         }
     }
 
+    IEnumerator ExampleCoroutine(bool triggerTest) {
+        //Print the time of when the function is first called.
+        Debug.Log("Started Coroutine at timestamp : " + Time.time);
+
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(1.5f);
+        isActive = triggerTest;
+        //After we have waited 5 seconds print the time again.
+        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+    }
+
     // Update is called once per frame
     void Update()
     {
         if (isActive) {
+            
             if (currentWanderPoint.position.x - m_transform.position.x > 0)
                 moveDirection = +1;
             else if (currentWanderPoint.position.x - m_transform.position.x < 0)
@@ -61,14 +76,14 @@ public class GolemBehaviour : CrystalBase
             currentWanderPoint = wanderPoints[wanderPointIndex];
         }
     }
-
     public override void OnTriggerCrystal() {
 
         if (isTriggered)
             return;
         
         isTriggered = true;
-        isActive = isTriggered;
+        StartCoroutine(ExampleCoroutine(isTriggered));
+        //isActive = isTriggered;
 
     }
     public override void OnReleaseCrystal() {
