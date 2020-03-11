92ffd7601d64e7d2e789cbeefb6701b27e658acd
Karl Baecklund <Karl.baecklund@gmail.com>
2020-03-11 13:24:02 +0100

    Golem tweak

--- Ruins-Of-Might/Ruins-Of-Might/Assets/Scripts/GolemBehaviour.cs
+++ Ruins-Of-Might/Ruins-Of-Might/Assets/Scripts/GolemBehaviour.cs
@@ -4,86 +4,121 @@ using UnityEngine;
 
 public class GolemBehaviour : CrystalBase
 {
-    [SerializeField] float m_speed = 10f;
-
-    [SerializeField] Transform[] wanderPoints;
-    [SerializeField] Transform currentWanderPoint;
-    [SerializeField] LayerMask pickupable;
-    [SerializeField] int wanderPointIndex;
     [SerializeField] bool isActive;
-    private int indexDirection;
+    [SerializeField] float m_speed;
+
+    [SerializeField] LayerMask m_layer;
+
+    [SerializeField] Vector2 endOffSet;
+    [SerializeField] float startOffSetX;
+    [SerializeField] float startOffSetY;
+
+    [SerializeField] Animator m_animator = null;
+
+    private Rigidbody2D m_rigidbody = null;
+
+    private Vector2 startCast;
+    private Vector2 endCast = Vector2.zero;
+
+    [SerializeField] Transform m_pickUpPos = null;
+    [SerializeField] Transform m_dropOffPos = null;
+
+    private bool movingRight = true;
     private int moveDirection;
+    private SpriteRenderer sprite;
 
-    [SerializeField] Rigidbody2D m_rigidbody;
-    [SerializeField] Transform m_transform;
-    // Start is called before the first frame update
-
+    [Header("Animator parameters")]
+    [SerializeField] string animRunningBool;
+    [SerializeField] string animJumpStartTrigger;
+    [SerializeField] string animGroundedBool;
     void Start()
     {
-        
         if (m_rigidbody == null) {
             m_rigidbody = GetComponent<Rigidbody2D>();
         }
-
-        if (m_transform == null) {
-            m_transform = GetComponent<Transform>();
+        if (sprite == null) {
+            sprite = GetComponent<SpriteRenderer>();
         }
 
-        if (currentWanderPoint == null) {
-            currentWanderPoint = wanderPoints[0];
+        if (movingRight == true) {
+            moveDirection = +1;
+            movingRight = false;
         }
     }
-
     IEnumerator ExampleCoroutine(bool triggerTest) {
         //Print the time of when the function is first called.
         Debug.Log("Started Coroutine at timestamp : " + Time.time);
-
-        //yield on a new YieldInstruction that waits for 5 seconds.
+        //yield on a new YieldInstruction that waits for 1.5 seconds.
         yield return new WaitForSeconds(1.5f);
         isActive = triggerTest;
-        //After we have waited 5 seconds print the time again.
+        //After we have waited 1.5 seconds print the time again.
         Debug.Log("Finished Coroutine at timestamp : " + Time.time);
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (isActive) {
-            
-            if (currentWanderPoint.position.x - m_transform.position.x > 0)
-                moveDirection = +1;
-            else if (currentWanderPoint.position.x - m_transform.position.x < 0)
-                moveDirection = -1;
-            Debug.Log(moveDirection * m_speed);
-        }
     }
-
     private void FixedUpdate() {
-        if(isActive)
+        if (isActive) {
+            startCast = transform.position;
+            startCast.y += startOffSetY;
+            RaycastHit2D hit;
+            if (movingRight == true) {
+               
+                startCast.x -= startOffSetX;
+            }
+            else {
+                startCast.x += startOffSetX;
+                
+            }
+            endCast = endOffSet;
+            Debug.DrawRay(startCast, endCast, Color.red);
+            hit = Physics2D.Linecast(startCast, endCast, m_layer);
+
+            if (hit.collider != null && hit.collider != this) {
+                if (hit.collider.gameObject.layer.Equals(0)) {
+                    if (movingRight == true) {
+                        moveDirection = +1;
+                        sprite.flipX = false;
+                        //transform.localScale = new Vector2(8, 8);
+                        movingRight = false;
+                    }
+                    else {
+                        moveDirection = -1;
+                        sprite.flipX = true;
+                        //transform.localScale = new Vector2(-8, 8);
+                        movingRight = true;
+                    }
+                }
+            }
+            if (hit.collider != null && hit.collider != this) {
+                if (hit.collider.gameObject.layer.Equals(10)) {
+                    
+                    hit.collider.transform.position = m_pickUpPos.position;
+                    hit.collider.transform.parent = gameObject.transform;
+
+                    hit.collider.GetComponent<CrystalBase>().OnTriggerCrystal();
+                    hit.collider.GetComponent<BoxCollider2D>().enabled = false;
+                }
+            }
             m_rigidbody.velocity = new Vector2(moveDirection * m_speed, m_rigidbody.velocity.y);
-        
-    }
+        }
+        if (isActive == false) {
+            if(transform.childCount > 2) {
+                var childBox2d = transform.GetChild(2);
 
-    private void OnTriggerEnter2D(Collider2D collision) {
-        Debug.Log("change");
-        if (collision.transform == currentWanderPoint) {
-            if (wanderPointIndex <= 0)
-                indexDirection = 1;
-            if (wanderPointIndex >= wanderPoints.Length -1)
-                indexDirection = -1;
-
-            wanderPointIndex += indexDirection;
-            currentWanderPoint = wanderPoints[wanderPointIndex];
+                childBox2d.GetComponent<BoxCollider2D>().enabled = true;
+                childBox2d.GetComponent<CrystalBase>().OnReleaseCrystal();
+                childBox2d.transform.position = m_dropOffPos.position;
+                Transform child = transform.GetChild(2).GetComponent<Transform>();
+                child.parent = null;
+            }
         }
     }
     public override void OnTriggerCrystal() {
 
         if (isTriggered)
             return;
-        
+
         isTriggered = true;
         StartCoroutine(ExampleCoroutine(isTriggered));
-        //isActive = isTriggered;
 
     }
     public override void OnReleaseCrystal() {
