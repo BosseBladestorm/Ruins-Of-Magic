d79a5876805072e51a48fbbb4ac0e3aa17c7316b
Elias Herman <elias.herman01@gmail.com>
2020-03-16 17:13:10 +0100

    Wood blocks now burn (wrong branch fak)

--- Ruins-Of-Might/Ruins-Of-Might/Assets/Scripts/CrystalBehaviour/IceBehaviour.cs
+++ Ruins-Of-Might/Ruins-Of-Might/Assets/Scripts/CrystalBehaviour/IceBehaviour.cs
@@ -6,12 +6,14 @@ public class IceBehaviour : MonoBehaviour {
 
     [SerializeField] LayerMask m_layerMask;
     [SerializeField] PhysicsMaterial2D m_matFrozen;
+
     private void OnTriggerEnter2D(Collider2D collider) {
         var hit = Physics2D.Linecast(this.transform.position, collider.transform.position, m_layerMask);
         if (hit) {
             Freezing(hit.transform.gameObject);
         }
     }
+
     private void Freezing(GameObject obj) {
 
         obj.GetComponent<BoxCollider2D>().sharedMaterial = m_matFrozen;
