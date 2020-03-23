693457b8852d7a114a1fc9f4f6ffabe8c1f00078
AmandaJern <amandajerne@gmail.com>
2020-03-23 14:44:56 +0100

    Credits tillagt

--- Ruins-Of-Might/Ruins-Of-Might/Assets/Scripts/CreditsScript.cs
+++ Ruins-Of-Might/Ruins-Of-Might/Assets/Scripts/CreditsScript.cs
@@ -2,34 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class CreditsScript : MonoBehaviour
 {
-    [SerializeField] bool continuous = true;
+    [Header("Settings")]
+    [SerializeField]
+    [Tooltip("Name of the next scene to play.")]
+    string nextScene = "";
+
     [SerializeField] float fadeSpeed = 1f;
     [SerializeField] float imageAliveTime = 2f;
     [SerializeField] float timeBetweenImages = 0.5f;
+
+    [Header("Slide Show Content")]
     [SerializeField] Image[] creditsImages;
     int imageIndex = 0;
 
 
 
-    private void Start()
-    {
-        if (continuous) {
+    private void Start() {
             StartCoroutine(FadeContinuous(imageAliveTime, fadeSpeed, timeBetweenImages, creditsImages));
-        } else {
-            StartCoroutine(FadeIn(fadeSpeed, creditsImages[0]));
-        }
-    }
-
-    private void Update() {
-        if (Input.GetButtonDown("Fire1") && imageIndex <= creditsImages.Length) {
-            StartCoroutine(NextImage(fadeSpeed, timeBetweenImages, creditsImages[imageIndex]));
-            imageIndex++;
-        } else if (imageIndex > creditsImages.Length) {
-            imageIndex = 0;
-        }
     }
 
     private IEnumerator FadeContinuous(float imageAliveTime, float fadeSpeed, float timeBetweenImages, Image[] creditsImages)
@@ -40,13 +33,7 @@ public class CreditsScript : MonoBehaviour
             yield return FadeOut(fadeSpeed, creditsImages[i]);
             yield return new WaitForSeconds(timeBetweenImages);
         }
-    }    
-    
-    private IEnumerator NextImage(float fadeSpeed, float timeBetweenImages, Image img)
-    {
-        yield return FadeOut(fadeSpeed, img);
-        yield return new WaitForSeconds(timeBetweenImages);
-        yield return FadeIn(fadeSpeed, img);
+        SceneManager.LoadScene(nextScene);
     }
 
     private IEnumerator FadeIn(float fadeSpeed, Image img)
